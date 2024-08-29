4ab99c92be2a3b4a463910405c76e2e99c108e21
berbascum <berbascum@ticv.cat>
2024-08-30 00:27:44 +0200

    (droidian) kernel-info: disable by default DEVEL and DEBUG fragments

--- debian/kernel-info.mk
+++ debian/kernel-info.mk
@@ -33,10 +33,10 @@ KERNEL_CONFIG_USE_FRAGMENTS = 1
 # Enable kernel config device extra fragments
 KERNEL_CONFIG_USE_EXTRA_FRAGMENTS = 1
 KERNEL_CONFIG_EXTRA_FRAGMENTS = vayu-halium.config
-KERNEL_CONFIG_USE_DEVEL_FRAGMENTS = 1
-KERNEL_CONFIG_DEVEL_FRAGMENTS = vayu-test.config vayu-develop.config
-KERNEL_CONFIG_USE_DEBUG_FRAGMENTS = 1
-KERNEL_CONFIG_DEBUG_FRAGMENTS = vayu-debug.config common_fragments/debug.config
+KERNEL_CONFIG_USE_DEVEL_FRAGMENTS = 0
+# KERNEL_CONFIG_DEVEL_FRAGMENTS = vayu-test.config vayu-develop.config
+KERNEL_CONFIG_USE_DEBUG_FRAGMENTS = 0
+# KERNEL_CONFIG_DEBUG_FRAGMENTS = vayu-debug.config common_fragments/debug.config
 
 # Whether to use diffconfig to generate the device-specific configuration.
 # If you enable this, you should set KERNEL_CONFIG_USE_FRAGMENTS to 1.
