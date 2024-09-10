1193058f88259ee77e1146db558adb95c40985fa
berbascum <berbascum@ticv.cat>
2024-09-10 21:31:01 +0200

    (droidian) kernel-info: disable the EXTRA_FRAGMENTS var

--- debian/kernel-info.mk
+++ debian/kernel-info.mk
@@ -31,7 +31,7 @@ DEVICE_FULL_NAME = Xiaomi Pocophone X3 Pro
 KERNEL_CONFIG_USE_FRAGMENTS = 1
 
 # Enable kernel config device extra fragments
-KERNEL_CONFIG_USE_EXTRA_FRAGMENTS = 1
+KERNEL_CONFIG_USE_EXTRA_FRAGMENTS = 0
 KERNEL_CONFIG_EXTRA_FRAGMENTS = vayu-droidian.config vayu-halium.config vayu-optimizations.config #vayu-halium-extended.config #container.config
 KERNEL_CONFIG_USE_DEVEL_FRAGMENTS = 0
 # KERNEL_CONFIG_DEVEL_FRAGMENTS = vayu-test.config vayu-develop.config
