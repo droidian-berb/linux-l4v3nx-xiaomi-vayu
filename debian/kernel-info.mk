1acb13d52dee0daf1f08c588cd892619cfe285fe
berbascum <berbascum@ticv.cat>
2024-11-27 20:30:10 +0100

    (clang) kernel-info: improve the clang version configuration

--- debian/kernel-info.mk
+++ debian/kernel-info.mk
@@ -210,14 +210,23 @@ BUILD_CC = clang
 # Use llvm instead of gcc. Recent Android kernels (past android 12) need llvm.
 BUILD_LLVM = 1
 
+# Set the Droidian official clang version
+# CLANG_VERSION = 6.0-4691093
+# CLANG_VERSION = 9.0-r353983c
+CLANG_VERSION = 10.0-r370808
+# CLANG_VERSION = 11.0-r383902
+# CLANG_VERSION = 12.0-r416183b
+# CLANG_VERSION = 14.0-r450784d
+# CLANG_VERSION = 17.0-r487747
+
 # Extra paths to prepend to the PATH variable. You'll probably want
 # to specify the clang path here (the default).
-BUILD_PATH = /usr/lib/llvm-android-10.0-r370808/bin
+BUILD_PATH = /usr/lib/llvm-android-$(CLANG_VERSION)/bin
 
 # Extra packages to add to the Build-Depends section. Mainline builds
 # can have this section empty, unless cross-building.
 # The default is enough to install the Android toolchain, including clang.
-DEB_TOOLCHAIN = linux-initramfs-halium-generic:arm64, binutils-aarch64-linux-gnu, clang-android-10.0-r370808, gcc-4.9-aarch64-linux-android, g++-4.9-aarch64-linux-android, libgcc-4.9-dev-aarch64-linux-android-cross
+DEB_TOOLCHAIN = linux-initramfs-halium-generic:arm64, binutils-aarch64-linux-gnu, clang-android-$(CLANG_VERSION), gcc-4.9-aarch64-linux-android, g++-4.9-aarch64-linux-android, libgcc-4.9-dev-aarch64-linux-android-cross
 
 # Where we're building on
 DEB_BUILD_ON = amd64
