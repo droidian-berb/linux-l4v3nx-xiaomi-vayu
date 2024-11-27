da6646448e9e5a4029bf42c3fdc31839c8f886d7
berbascum <berbascum@ticv.cat>
2024-11-27 20:18:25 +0100

    (extra-fragments) kernel-info: add support for config extra fragments, enabling vayu-droidian, container and vayu-debug

--- debian/kernel-info.mk
+++ debian/kernel-info.mk
@@ -30,6 +30,16 @@ DEVICE_FULL_NAME = Xiaomi Pocophone X3 Pro
 # If unsure, keep this to 0.
 KERNEL_CONFIG_USE_FRAGMENTS = 1
 
+# Enable kernel config device extra fragments
+KERNEL_CONFIG_USE_EXTRA_FRAGMENTS = 1
+KERNEL_CONFIG_EXTRA_FRAGMENTS = vayu-droidian.config container.config
+## Development fragments
+   # vayu-optimizations.config vayu-halium.config  #vayu-halium-lxc.config vayu-halium-net.config vayu-halium-security.config
+KERNEL_CONFIG_USE_DEVEL_FRAGMENTS = 0
+# KERNEL_CONFIG_DEVEL_FRAGMENTS = vayu-test.config vayu-develop.config
+KERNEL_CONFIG_USE_DEBUG_FRAGMENTS = 1
+KERNEL_CONFIG_DEBUG_FRAGMENTS = vayu-debug.config #common_fragments/debug.config
+
 # Whether to use diffconfig to generate the device-specific configuration.
 # If you enable this, you should set KERNEL_CONFIG_USE_FRAGMENTS to 1.
 # If unsure, keep this to 0.
