386381b852d8d4b7e413742101b765c458806679
berbascum <berbascum@ticv.cat>
2024-12-01 17:28:38 +0100

    (cmdline) kernel-info: add luks.cust.dev for testing

--- debian/kernel-info.mk
+++ debian/kernel-info.mk
@@ -11,7 +11,7 @@ VARIANT = android
 KERNEL_BASE_VERSION = 4.14-0
 
 # The kernel cmdline to use
-KERNEL_BOOTIMAGE_CMDLINE = androidboot.hardware=qcom androidboot.console=ttyMSM0 androidboot.memcg=1 lpm_levels.sleep_disabled=1 video=vfb:640x400,bpp=32,memsize=3072000 msm_rtb.filter=0x237 service_locator.enable=1 swiotlb=2048 loop.max_part=7 androidboot.usbcontroller=a600000.dwc3 console=tty0 console=ttyMSM0,115200n8 systemd.unified_cgroup_hierarchy=0 buildvariant=user
+KERNEL_BOOTIMAGE_CMDLINE = androidboot.hardware=qcom androidboot.console=ttyMSM0 androidboot.memcg=1 lpm_levels.sleep_disabled=1 video=vfb:640x400,bpp=32,memsize=3072000 msm_rtb.filter=0x237 service_locator.enable=1 swiotlb=2048 loop.max_part=7 androidboot.usbcontroller=a600000.dwc3 console=tty0 console=ttyMSM0,115200n8 systemd.unified_cgroup_hierarchy=0 buildvariant=user luks.cust.dev=/dev/mmcblk0p4
 # In development, early access available
 # droidian.lvm.noresize luks.cust.dev=/dev/mmcblk0p4
 # Boot params for debug
